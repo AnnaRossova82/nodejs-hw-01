b1a408c19543b42614297fc5ba560b431effc7e3
AnnaRossova <rossova.anna@gmail.com>
2024-06-07 23:30:39 +0300

    created

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  try {
+      let contacts = [];
+    try {
+      const data = await fs.readFile(PATH_DB, 'utf-8');
+      contacts = JSON.parse(data);
+    } catch (error) {
+      if (error.code !== 'ENOENT') throw error;
+    }
+
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    console.log('New contact added:', newContact);
+  } catch (error) {
+    console.error('Error adding contact:', error);
+  }
+};
+
 
 await addOneContact();
--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+  
+    const emptyContactsArray = [];
+    
+    await fs.writeFile(PATH_DB, JSON.stringify(emptyContactsArray, null, 2));
+    
+    console.log('All contacts have been removed.');
+  } catch (error) {
+    console.error('Error removing contacts:', error);
+  }
+};
 
 await removeAllContacts();
