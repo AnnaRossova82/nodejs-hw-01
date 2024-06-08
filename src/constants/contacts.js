aa7aabb3fc38dc287bac9e5aeb0235606f517d2f
AnnaRossova <rossova.anna@gmail.com>
2024-06-08 17:38:06 +0300

    add path

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,2 +1,31 @@
-const PATH_DB = './src/db/db.json';
+import path from 'node:path';
+
+
+const PATH_DB = path.resolve(process.cwd(), 'src', 'db', 'db.json');
 export { PATH_DB };
+
+
+
+
+
+
+/* 
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PATH_DB = path.join(__dirname, '..', 'db', 'db.json');
+console.log(PATH_DB); 
+export { PATH_DB };
+
+
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PATH_DB = path.resolve(__dirname, '..', 'db', 'db.json');
+export { PATH_DB }; */
